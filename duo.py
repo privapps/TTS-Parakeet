0cc71b8a32076d55acc3b87ef65fcc6fb9fcdb99
privapps                                                                                                                                                                                           email = privapps@gmx.com <jing.xiao@verint.com>
2021-07-24 11:57:37 -0700

    chaos

--- duo.py
+++ duo.py
@@ -57,7 +57,7 @@ def one_by_one(line, arr):
     if len(text)<=1 or not re.search(r'[a-zA-Z0-9]+', text):
         return
     long_words = [wrd for wrd in nltk.word_tokenize(text) if len(wrd) > 3]
-    if len(long_words) > 3 and not re.search(r'([A-Z]\.)+', text) and not re.search(r'Chin(a|ese)', text) and not re.search(r'(?i)focus(es|ing)', text) and not re.search(r'(?i)(rational|anger|imagine|island)', text) and text.find('é') < 0:
+    if len(long_words) > 3 and not re.search(r'([A-Z]\.)+', text) and not re.search(r'Chin(a|ese)', text) and not re.search(r'(?i)focus(es|ing)', text) and not re.search(r'(?i)(rational|anger|imagine|island)', text) and not text.find('é') < 0 and re.search(r'(?i)chaos', text):
         print('debug: ',text, nltk.word_tokenize(text))
         audio_data = _tacotron2_one(text)
     else:
